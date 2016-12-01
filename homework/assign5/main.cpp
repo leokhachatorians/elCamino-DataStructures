3610a481be7c9b6462363c9bb59d500e431b2c15
leokhachatorians <leokhachatorians@gmail.com>
2016-11-30 23:13:12 -0800

    Trying to figure out this crap

--- homework/assign5/main.cpp
+++ homework/assign5/main.cpp
@@ -27,56 +27,56 @@ int main() {
 
     for (vector<char>::iterator it = v.begin(); it != v.end(); ++it) {
         if (*it == '(') { push_to_stack_flow(balance_stack, *it); }
-        if (*it == '[') { push_to_stack_flow(balance_stack, *it); }
-        if (*it == '{') { push_to_stack_flow(balance_stack, *it); }
-        if (*it == '"') { push_to_stack_flow(balance_stack, *it); }
-        if (*it == '\'') { push_to_stack_flow(balance_stack, *it); }
-        if (*it == '/') { 
+        else if (*it == '[') { push_to_stack_flow(balance_stack, *it); }
+        else if (*it == '{') { push_to_stack_flow(balance_stack, *it); }
+        else if (*it == '"') { push_to_stack_flow(balance_stack, *it); }
+        else if (*it == '\'') { push_to_stack_flow(balance_stack, *it); }
+        else if (*it == '/') { 
             it++;
             if (*it == '*') { push_to_stack_flow(balance_stack, 'B'); }
             it--;
         }
 
-        //if (balance_stack.size() > 0 && (balance_stack.top() != 'B' && balance_stack.top() != '"' && balance_stack.top() != '\'')) {
-        if (*it == ')' && balance_stack.top() != 'B' && balance_stack.top() != '"' && balance_stack.top() != '\'') {
-            if (balance_stack.top() != '(') {
-                cout << "error unbalanced ()" << endl;
+        if (balance_stack.size() > 0 && (balance_stack.top() != 'B' || balance_stack.top() != '"' || balance_stack.top() != '\'')) {
+            if (*it == ')') {
+                if (balance_stack.top() != '(') {
+                    cout << "error unbalanced ()" << endl;
+                }
+                else
+                    balance_stack.pop();
             }
-            else
-                balance_stack.pop();
-        }
-        if (*it == '}' && balance_stack.top() != 'B' && balance_stack.top() != '"' && balance_stack.top() != '\'') {
-            if (balance_stack.top() != '{') {
-                cout << balance_stack.top() << endl;
-                cout << "error unbalanced {}" << endl;
+            else if (*it == '}') {
+                if (balance_stack.top() != '{') {
+                    cout << balance_stack.top() << endl;
+                    cout << "error unbalanced {}" << endl;
+                }
+                else
+                    balance_stack.pop();
             }
-            else
-                balance_stack.pop();
-        }
-        if (*it == ']' && balance_stack.top() != 'B' && balance_stack.top() != '"' && balance_stack.top() != '\'') {
-            if (balance_stack.top() != '[') {
-                cout << balance_stack.top() << endl;
-                cout << "error unbalanced []" << endl;
+            else if (*it == ']') {
+                if (balance_stack.top() != '[') {
+                    cout << balance_stack.top() << endl;
+                    cout << "error unbalanced []" << endl;
+                }
+                else
+                    balance_stack.pop();
             }
-            else
-                balance_stack.pop();
-        }
-        if (*it == '"' && balance_stack.top() != 'B' && balance_stack.top() != '\'') {
-            if (balance_stack.top() != '"') {
-                cout << "error unbalanced \"" << endl;
+            else if (*it == '"') {
+                if (balance_stack.top() != '"') {
+                    cout << "error unbalanced \"" << endl;
+                }
+                else
+                    balance_stack.pop();
             }
-            else
-                balance_stack.pop();
-        }
-        if (*it == '\'' && balance_stack.top() != 'B' && balance_stack.top() != '"') {
-            if (balance_stack.top() != '\'') {
-                cout << "error unbalanced ''" << endl;
+            else if (*it == '\'') {
+                if (balance_stack.top() != '\'') {
+                    cout << "error unbalanced ''" << endl;
+                }
+                else
+                    balance_stack.pop();
             }
-            else
-                balance_stack.pop();
         }
-        //}
-        if (*it == '*') {
+        else if (*it == '*') {
             it++;
             if (*it == '/') {
                 if (balance_stack.top() != 'B') {
@@ -100,23 +100,6 @@ int main() {
     return 0;
 }
 
-//bool recursive_dive(stack<char> stack, vector<char>::iterator it, char end_sym, const vector<char> v) {
-//    bool found = false;
-//    if (it == v.end()) {
-//        return false;
-//    }
-//
-//    while (++it != v.end() || found) {
-//        if (*it == end_sym) {
-//            found = true;
-//        }
-//        switch (*it) {
-//            case '['
-//
-//
-//
-//}
-
 
 bool check_if_empty(stack<char> &stack) {
     if (stack.size() < 1) {
@@ -126,10 +109,10 @@ bool check_if_empty(stack<char> &stack) {
 }
 
 bool check_if_in_string_or_block_comment(stack<char> &stack) {
-    if (stack.top() == '"' && stack.top() == 'B' && stack.top() == '\'') {
-        return false;
+    if (stack.top() == '"' || stack.top() == 'B' || stack.top() == '\'') {
+        return true;
     }
-    return true;
+    return false;
 }
 
 void push_to_stack_flow(stack<char> &stack, char c) {
@@ -139,7 +122,7 @@ void push_to_stack_flow(stack<char> &stack, char c) {
     }
     else {
         cout << "Before: " << c << " " << stack.top() << " " << check_if_in_string_or_block_comment(stack) << endl;
-        if (check_if_in_string_or_block_comment(stack)) {
+        if (!check_if_in_string_or_block_comment(stack)) {
                 stack.push(c);
         }
         cout << "After: " << c << " " << stack.top() << " " << check_if_in_string_or_block_comment(stack) << "\n" << endl;
